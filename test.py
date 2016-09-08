df456dc2d700585065280be48d28024167a3f6e5
Kieran <dong2409@vandals.uidaho.edu>
2016-09-08 14:56:41 -0700

    simple python script
    Says "45" at the end.

---
+++ test.py
@@ -0,0 +1,18 @@
+#a list of four
+for i in range (10):
+	print ( i )
+	if i == 4:
+		print ("where")
+#a list of five
+myList = [ 1, 2, 3, 4, 5]
+#for a long comment, three periods
+
+i = 0
+total = 0
+while i < 10:
+	total = total + i
+	i = i + 1
+	
+print ( total )
+
+myList = [ 0, 1, 2, 3, 4, ]
